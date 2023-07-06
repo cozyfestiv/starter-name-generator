82b666d4d2e7bec7afbc39119763e1820d8c7a70
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-07-06 12:13:23 -0400

    updating fetch URL

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([]);
 
   useEffect(() => {
-    fetch('https://name-generator-static.onrender.com/')
+    fetch('https://server-deploy-miik.onrender.com/generate')
       .then(res => res.json())
       .then(nme => setName([nme]));
   }, []);
